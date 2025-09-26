6fd2face1cbb7ad144d7e2b5a0f0b147d42e7a20
lakshman <lakshman_1241@yahoo.in>
2025-09-26 16:13:58 +0100

    close signup modal before login

--- cypress/support/step_definitions/authentication.steps.js
+++ cypress/support/step_definitions/authentication.steps.js
@@ -37,10 +37,17 @@ When('I register a new unique user', () => {
 
   cy.get(selectors.signupSubmit).click();
   cy.get('@alert').should('have.been.called');
+  // Close the Sign up modal to avoid covering the Login link
+  cy.get('#signInModal').within(() => {
+    cy.contains('button', 'Close').click({ force: true });
+  });
+  cy.get('#signInModal').should('not.be.visible');
 });
 
 When('I log in with the registered user', () => {
   cy.get('@creds').then(({ user, pass }) => {
+    // Ensure Sign up modal is closed and not covering the login button
+    cy.get('#signInModal').should('not.be.visible');
     cy.get(selectors.loginBtn).click();
     cy.get(selectors.loginUsername).should('be.visible').type(user);
     cy.get(selectors.loginPassword).type(pass);
