4897a88f5366dcc6f4f34855206e866633f56acb
lakshman <lakshman_1241@yahoo.in>
2025-09-26 14:07:38 +0100

    basic framework initial save

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "cypress/e2e/**/*.feature",
+    baseUrl: "https://www.demoblaze.com",
+    supportFile: "cypress/support/e2e.js",
+    viewportWidth: 1366,
+    viewportHeight: 768,
+    chromeWebSecurity: false,
+    video: true,
+    screenshotOnRunFailure: true,
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
+    },
+  },
+});
---
+++ cypress/e2e/step_definitions/authentication.steps.js
@@ -0,0 +1,59 @@
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+
+const selectors = {
+  signupBtn: '#signin2',
+  signupUsername: '#sign-username',
+  signupPassword: '#sign-password',
+  signupSubmit: 'button[onclick="register()"]',
+  loginBtn: '#login2',
+  loginUsername: '#loginusername',
+  loginPassword: '#loginpassword',
+  loginSubmit: 'button[onclick="logIn()"]',
+  navbarWelcome: '#nameofuser'
+};
+
+function uniqueUser() {
+  const ts = Date.now();
+  const user = `testuser_${ts}`;
+  const pass = `SecurePass_${ts}!`;
+  return { user, pass };
+}
+
+Given('I navigate to the DemoBlaze homepage', () => {
+  cy.visit('/index.html');
+});
+
+When('I register a new unique user', () => {
+  const creds = uniqueUser();
+  cy.wrap(creds).as('creds');
+
+  cy.get(selectors.signupBtn).click();
+  cy.get(selectors.signupUsername).should('be.visible').type(creds.user);
+  cy.get(selectors.signupPassword).type(creds.pass);
+
+  cy.window().then((win) => {
+    cy.stub(win, 'alert').as('alert');
+  });
+
+  cy.get(selectors.signupSubmit).click();
+  cy.get('@alert').should('have.been.called');
+});
+
+When('I log in with the registered user', () => {
+  cy.get('@creds').then(({ user, pass }) => {
+    cy.get(selectors.loginBtn).click();
+    cy.get(selectors.loginUsername).should('be.visible').type(user);
+    cy.get(selectors.loginPassword).type(pass);
+    cy.get(selectors.loginSubmit).click();
+  });
+});
+
+Then('I should be successfully logged in', () => {
+  cy.get(selectors.navbarWelcome, { timeout: 10000 }).should('be.visible');
+});
+
+Then('I should see the welcome message for the registered user', () => {
+  cy.get('@creds').then(({ user }) => {
+    cy.get(selectors.navbarWelcome).should('contain.text', `Welcome ${user}`);
+  });
+});
